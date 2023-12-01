ff2b26239dd53a2889afdabac29cb5dcfe6d0ef4
Aadarsh Guru <112774513+Aadarsh-guru@users.noreply.github.com>
2023-12-01 10:00:37 +0530

    Update use-chat-query.ts

--- hooks/use-chat-query.ts
+++ hooks/use-chat-query.ts
@@ -40,7 +40,7 @@ export const useChatQuery = ({
         queryKey: [queryKey],
         queryFn: fetchMessages,
         getNextPageParam: (lastPage) => lastPage?.nextCursor,
-        refetchInterval: 100,
+        refetchInterval: 1000,
     });
 
     return {
